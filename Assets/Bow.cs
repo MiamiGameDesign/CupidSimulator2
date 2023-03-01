2014c4ab93da729290c2b9d579b068cd1b89c26b
MiamiGameDesign <124742643+MiamiGameDesign@users.noreply.github.com>
2023-02-28 19:56:41 -0500

    Cylinder replaced with real enemy

--- Assets/Bow.cs
+++ Assets/Bow.cs
@@ -36,8 +36,10 @@ public class Bow : MonoBehaviour
             Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
             RaycastHit hit;
             Vector3 targetPoint;
-            if(Physics.Raycast(ray, out hit))
+            if(Physics.Raycast(ray, out hit)) {
                 targetPoint = hit.point;
+                //Destroy(arrow);
+            }
             else
                 targetPoint = ray.GetPoint(69);
             Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
@@ -80,6 +82,6 @@ public class Bow : MonoBehaviour
     }
     void OnCollisonEnter(Collider other)
     {
-        Destroy(gameObject);
+        Destroy(arrow);
     }
 }
